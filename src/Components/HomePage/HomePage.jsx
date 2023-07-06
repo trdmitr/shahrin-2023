890982507ee60f3aa1bc00c1f8e58d97cc68c295
trdmitr <trdmitr@gmail.com>
2023-07-07 00:37:04 +0400

    css fin

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import classes from "./HomePage.module.css"
 import { Link } from 'react-router-dom';
 
 export const HomePage = () => {
-  const imgUrl = "https://sun9-2.userapi.com/impg/SnPF98mn-hy2gKcjpfQQrMwqV-Os3g4AYWZbbw/sTSqtWo79pw.jpg?size=1600x1783&quality=96&sign=b276a595d27c6aed40201dd821fef8d5&type=album"
+  const imgUrl = "https://storage.googleapis.com/glide-prod.appspot.com/uploads-v2/GIN8bv7MW6cA88DoCZaq/pub/S9Dx7tDnccE1vw524oqt.jpg"
   // const imgUrl2 = 'https://sun2-22.userapi.com/impg/2YiE2IW9r0friRMzAEniG_oR87ffPUfqKRswKw/vtMiX4nUf8o.jpg?size=300x300&quality=96&sign=7757c3bd97980caf5674faa89f7781c3&type=album'
   const imgAlt = "Каверы Подземки"
   // const frontAudio = "https://drive.google.com/uc?export=download&id=1U3M7aGhmpFQHd3SNNupwghkrQagbRKq2";
@@ -15,7 +15,7 @@ export const HomePage = () => {
 
                 <div>
                     {/* <span className={classes.navLink}>Сборник за 3 года</span> */}
-                    <Link to="/cavers21"  className={classes.navLink21}>2021</Link>
+                    {/* <Link to="/cavers21"  className={classes.navLink21}>2021</Link> */}
                     <Link to="/cavers22"  className={classes.navLink22}>2022</Link>
                     <Link to="/cavers23"  className={classes.navLink23}>2023</Link>
     
